179f120d5dbe9ca73517c934b19df5c2249cb469
shivam mishra <shivam@depasserinfotech.in>
2023-09-20 11:29:05 +0530

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    '@/assets/defaults.scss'
+  ],
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+})
---
+++ pages/movies/[id].vue
@@ -0,0 +1,121 @@
+<template>
+    <!-- <Loading v-if="isLoading"/> -->
+     <!-- Movie Info -->
+  <div class="single-movie container">
+    <NuxtLink class="button" :to="{ name: 'index' }"> Back </NuxtLink>
+    <div class="movie-info">
+      <div class="movie-img">
+        <img
+          :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`"
+          alt=""
+        />
+      </div>
+      <div class="movie-content">
+        <h1>Title: {{ movie.title }}</h1>
+        <p class="movie-fact tagline">
+          <span>Tagline:</span> "{{ movie.tagline }}"
+        </p>
+        <p class="movie-fact">
+          <span>Released:</span>
+          {{
+            new Date(movie.release_date).toLocaleString('en-us', {
+              month: 'long',
+              day: 'numeric',
+              year: 'numeric',
+            })
+          }}
+        </p>
+        <p class="movie-fact">
+          <span>Duration:</span> {{ movie.runtime }} minutes
+        </p>
+        <p class="movie-fact">
+          <span>Revenue:</span>
+          {{
+            movie.revenue.toLocaleString('en-us', {
+              style: 'currency',
+              currency: 'USD',
+            })
+          }}
+        </p>
+        <p class="movie-fact"><span>Overview:</span> {{ movie.overview }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+    const routerId = useRoute().params;
+    const delay = 2000; // Adjust the delay time (in milliseconds) as needed
+
+    // Simulate a loading delay before fetching data
+    await new Promise(resolve => setTimeout(resolve, delay));
+
+    const singleMovieData = await fetch(`https://api.themoviedb.org/3/movie/${routerId.id}?api_key=4ac565a58a0b2e3e45ea8689743f7564&language=en-US`);
+    const data = await singleMovieData.json();
+    const movie = ref('');
+    movie.value  = data
+
+</script>
+
+<style lang="scss" scoped>
+.single-movie {
+  color: #fff;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 32px 16px;
+
+  .button {
+    align-self: flex-start;
+    margin-bottom: 32px;
+  }
+
+  .movie-info {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 32px;
+    color: #fff;
+    @media (min-width: 800px) {
+      flex-direction: row;
+      align-items: flex-start;
+    }
+    .movie-img {
+      img {
+        max-height: 500px;
+        width: 100%;
+
+        @media (min-width: 800px) {
+          max-height: 700px;
+          width: initial;
+        }
+      }
+    }
+
+    .movie-content {
+      h1 {
+        font-size: 56px;
+        font-weight: 400;
+      }
+
+      .movie-fact {
+        margin-top: 12px;
+        font-size: 20px;
+        line-height: 1.5;
+
+        span {
+          font-weight: 600;
+          text-decoration: underline;
+        }
+      }
+
+      .tagline {
+        font-style: italic;
+        span {
+          font-style: normal;
+        }
+      }
+    }
+  }
+}
+</style>
