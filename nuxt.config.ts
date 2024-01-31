5580a783d9b1c01a524778a55e98cc2f6d94d5ff
Shivam Mishra <113275391+Shivammishra03@users.noreply.github.com>
2024-01-31 17:42:57 +0530

    dev tools enabled: false

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     '@/assets/defaults.scss'
